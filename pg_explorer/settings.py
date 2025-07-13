c50a0030066f0cefb2b334f74a66a26dd9240f67
Ivan Martin Vedriel <ivanprjcts@gmail.com>
2025-07-13 22:12:38 +0200

    in progress

---
+++ pg_explorer/settings.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-4$_=t*lg9+rf3v_h0gz)o@k-^uy!-^jvc2=^(@q&%@)o8!f+sf'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Application definition
+
+INSTALLED_APPS = [
+    'sample_app'
+]
+
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'postgres',
+        'PORT': 5432
+    }
+}
+
+
+# Internationalization
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
